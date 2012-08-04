dd1fdf4873c91f1615fba33c523f0f22a59847ef
Ilya Bagrak <ilya.bagrak@gmail.com>
2012-08-04 16:58:23 -0400

    Created browser products

--- web/js/script.js
+++ web/js/script.js
@@ -18,4 +18,19 @@ $(document).ready(function() {
     		$("#confirmphone").hide();
 		} 
 	});
+
+	$('.btn_more_info').click(function() {
+		var more_info = $(this).parents('.caption').children('.more-info').toggle();
+		if ($(this).html() === "More info") {
+			$(this).html('Less info');
+			$(this).removeClass('btn-primary');
+		} else {
+			$(this).html('More info');
+			$(this).addClass('btn-primary');
+		}
+	});
+
+	$('.btn_buy').click(function() {
+		window.location = 'account/orders.html';
+	});
 });
