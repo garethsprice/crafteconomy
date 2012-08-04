137c5237bbea1b0a453c03522ccddeac5eae5804
Ilya Bagrak <ilya.bagrak@gmail.com>
2012-08-04 15:36:55 -0400

    Create basic forms for login, register and sell

--- web/js/script.js
+++ web/js/script.js
@@ -2,4 +2,20 @@ $(document).ready(function() {
 	$('.carousel').carousel({
 		interval: 5000
 	});
+
+	$('#showbio').click(function () {
+   		if ($('#showbio').is(':checked')) {
+    		$("#makers_bio").show();
+		} else {
+    		$("#makers_bio").hide();
+		} 
+	});
+
+	$('#confirm_listing_sms').click(function () {
+   		if ($('#confirm_listing_sms').is(':checked')) {
+    		$("#confirmphone").show();
+		} else {
+    		$("#confirmphone").hide();
+		} 
+	});
 });
