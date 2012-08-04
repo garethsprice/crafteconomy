995263bd776a1ddf6da79986046698dd2a287cad
Gareth Price <gareth@readysetrocket.com>
2012-08-04 18:48:15 -0400

    Adding Google Analytics tracking

--- application/themes/crafteconomy/views/web/layouts/default.php
+++ application/themes/crafteconomy/views/web/layouts/default.php
@@ -25,6 +25,20 @@
 	<script src="<?php print site_url('assets/themes/crafteconomy/js/libs/modernizr-2.5.3.min.js'); ?>"></script>
 	
 	<?php echo $template['metadata']; ?>
+	
+	<script type="text/javascript">
+
+    var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', 'UA-33886838-1']);
+    _gaq.push(['_trackPageview']);
+
+    (function() {
+      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    })();
+
+  </script>
 </head>
 
 <!DOCTYPE html> 
