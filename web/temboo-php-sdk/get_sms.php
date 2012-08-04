1fb2a9438f154427da64e78365ee85fdd9cd64bc
abemassry <abe@massindustries.org>
2012-08-04 16:20:11 -0400

    replace code from temboo with twilio code for sms receive

--- web/temboo-php-sdk/get_sms.php
+++ web/temboo-php-sdk/get_sms.php
@@ -1,17 +1,7 @@
 <?php
-
-require 'src/temboo.php';
-
-// Instantiate a Temboo session with valid API key credentials
-$session = new Temboo_Session("crafteconomy", "crafteconomy", "e1cb547d-69ef-494a-b");
-
-// Instantiate the choreography using the session object
-$getSMSList = new Twilio_GetSMSList($session);
-
-// Get an input object for the choreo
-$inputs = $getSMSList->newInputs();
-
-// Execute choreography and get results
-$results = $getSMSList->execute($inputs)->getResults();
-
+	header("content-type: text/xml");
+	echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
 ?>
+<Response>
+	<Sms>Hello, Mobile Monkey</Sms>
+</Response>
