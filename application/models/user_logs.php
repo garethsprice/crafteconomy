46f6b227c961cdcd7b58bd1da7627ea1e414ef06
abemassry <abe@massindustries.org>
2012-08-04 16:56:32 -0400

    enable user log and sms receive writes to user log

---
+++ application/models/user_logs.php
@@ -0,0 +1,16 @@
+<?php
+class User_logs_model extends CI_Model {
+  public function __construct() {
+    $this->load->database();
+  }
+  
+  /**
+   * Save an entry to the database
+   */
+  public function write($text) {
+  	$data['log_message'] = $text;
+  	$DateOfRequest = date("Y-m-d H:i:s");
+  	$data['timestamp'] = $DateOfRequest;
+		return $this->db->insert('user_logs', $data);
+	}
+}
