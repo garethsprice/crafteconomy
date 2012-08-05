f9208c93a7b9218af4c34fc7b57e94128693e425
abemassry <abe@massindustries.org>
2012-08-04 22:09:33 -0400

    Orders start with payments and are updated by sms, edits to account and sms controller

--- application/controllers/account.php
+++ application/controllers/account.php
@@ -2,15 +2,25 @@
 
 class Account extends MY_Controller {
   public $login_required = TRUE;
+
+  public function __construct() {
+    parent::__construct();
+    $this->load->model('account_model');
+  }
   
-	public function orders()
-	{	  
-    $this->template->title('Orders');
-  	$this->template->build('account/orders');
-	}
+	// public function orders($order, $order_status)
+	// {	  
+	// 	$this->account_model->order_update($order, $order_status);
+ //    $this->template->title('Orders');
+ //  	$this->template->build('account/orders');
+
+
+	// }
 	
-	public function payments()
-	{	  
+	public function payments($product_id)
+	{	
+		$user = $this->ion_auth->user()->row();
+		$this->account_model->payment_start_order($product_id, $user);
     $this->template->title('Account Balance and Payments');
   	$this->template->build('account/payments');
 	}
--- application/controllers/sms.php
+++ application/controllers/sms.php
@@ -19,6 +19,7 @@ class Sms extends MY_Controller {
 	  //  $name = "Monkey";
 	  //}
 	  $this->load->model('user_logs');
+	  $this->load->model('account_model');
 	  $phone_number = $_REQUEST['From'];
 	  $sms_body = $_REQUEST['Body'];
 
@@ -28,23 +29,30 @@ class Sms extends MY_Controller {
 	  $reason = NULL;
 	  $tracking_number = NULL;
 	  $response_message = NULL;
+	  $order_status = NULL;
 
 	  if (preg_match('/OK/i', $sms_body)) {
 	  	$exp = explode(' ', $sms_body);
 	  	$action = 'OK';
+	  	$order_status = 'Accepted';
 	  	$order_id = $exp[1];
+	  	$this->account_model->order_update($order_id, $order_status);
 	  	$response_message = 'Thank you for accepting, you can ship the item at your earliest convenience.';
 	  } elseif (preg_match('/CANCEL/i', $sms_body)) {
 	  	$exp = explode(' ', $sms_body, 3);
 	  	$action = 'CANCEL';
+	  	$order_status = 'Cancelled';
 	  	$order_id = $exp[1];
 	  	$reason = $exp[2];
+	  	$this->account_model->order_update($order_id, $order_status);
 	  	$response_message = "We're sorry you had to cancel.";
 	  } elseif (preg_match('/SHIPPED/i', $sms_body)) {
 	  	$exp = explode(' ', $sms_body, 3);
 	  	$action = 'SHIPPED';
+	  	$order_status = 'Sent';
 	  	$order_id = $exp[1];
 	  	$tracking_number = $exp[2];
+	  	$this->account_model->order_update($order_id, $order_status);
 	  	$response_message = 'Thank you for sending! Funds will be distributed when the buyer receives the item.';
 	  } elseif (preg_match('/CRAFTHELP/i', $sms_body)) {
 	  	$response_message = 'Accept order: OK order number, Cancel order: CANCEL number reason, Send order: SHIPPED number Tracking-number';
