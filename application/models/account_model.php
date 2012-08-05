93e04c6f3e461ccf87e03ae00b80298b046fb6a1
Gareth Price <gareth@readysetrocket.com>
2012-08-05 02:14:50 -0400

    Sending received message when an order is received

--- application/models/account_model.php
+++ application/models/account_model.php
@@ -9,25 +9,26 @@ class Account_model extends CI_Model {
    */
   public function payment_start_order($product, $buyer) {   
     $buyer = $this->db->get_where('users', array('id' => $buyer))->row_array();
-
-    if($result) {
-  		$message = '<h1>Craft Economy</h1>
-    	<p>Your order was received and is processing.</p>
-    	<p>Thanks for using Craft Economy!</p>';
-  		$this->email->clear();
-  		$this->email->from($this->config->item('admin_email', 'ion_auth'), $this->config->item('site_title', 'ion_auth'));
-  		$this->email->to($buyer['email']);
-  		$this->email->subject('Receipt for your Craft Economy order (#' . $order['id'] . ')');
-  		$this->email->message($message);
-      $this->email->send();
-    }
      
   	$DateOfRequest = date("Y-m-d H:i:s");
     $data['product_id'] = $product;
   	$data['date_purchased'] = $DateOfRequest;
     $data['buyer_id'] = $buyer['id'];
     $data['order_status'] = 'Pending';
-		return $this->db->insert('orders', $data);
+    
+		$result = $this->db->insert('orders', $data);
+		
+		$message = '<h1>Craft Economy</h1>
+  	<p>Your order was received and is processing.</p>
+  	<p>Thanks for using Craft Economy!</p>';
+		$this->email->clear();
+		$this->email->from($this->config->item('admin_email', 'ion_auth'), $this->config->item('site_title', 'ion_auth'));
+		$this->email->to($buyer['email']);
+		$this->email->subject('Receipt for your Craft Economy order (#' . $this->db->insert_id() . ')');
+		$this->email->message($message);
+    $this->email->send();
+    
+    return $result;
 	}
 
   public function order_update($order, $order_status) {
@@ -47,7 +48,7 @@ class Account_model extends CI_Model {
   		$this->email->clear();
   		$this->email->from($this->config->item('admin_email', 'ion_auth'), $this->config->item('site_title', 'ion_auth'));
   		$this->email->to($buyer['email']);
-  		$this->email->subject('Your Craft Economy order (#' . $order['id'] . ')');
+  		$this->email->subject('Update for your Craft Economy order (#' . $order['id'] . ')');
   		$this->email->message($message);
       $this->email->send();
     }
