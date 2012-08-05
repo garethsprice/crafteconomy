afe9eac2ab67cd956f39547edc9519da43ef9d43
abemassry <abe@massindustries.org>
2012-08-04 21:21:03 -0400

    account model add, schema update

---
+++ application/models/account_model.php
@@ -0,0 +1,24 @@
+<?php
+class Account_model extends CI_Model {
+  public function __construct() {
+    $this->load->database();
+  }
+  
+  /**
+   * Save an entry to the database
+   */
+  public function payment_start_order($product, $buyer) {
+  	$DateOfRequest = date("Y-m-d H:i:s");
+    $data['product_id'] = $product;
+  	$data['date_purchased'] = $DateOfRequest;
+    $data['buyer_id'] = $buyer;
+    $data['order_status'] = 'Pending';
+		return $this->db->insert('orders', $data);
+	}
+
+  public function order_update($order, $order_status) {
+    $data['id'] = $order;
+    $data['order_status'] = $order_status;
+    return $this->db->insert('orders', $data);
+  }
+}
